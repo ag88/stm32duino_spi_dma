f1f6e16096d55e83f03bc61dc97f91a07cccfd64
ag88 <ag88@github.com>
2025-02-01 13:50:45 +0800

    rename ClassSPIx to SPIClassX

--- src/SPI/SPI.h
+++ src/SPI/SPI.h
@@ -11,54 +11,54 @@
 #if defined(STM32F4xx)
 #include "SPIDMA_F4XX.h"
 extern SPI_DMAF4 SPI;
-typedef SPI_DMAF4 ClassSPI1;
-typedef SPI_DMAF4_SPI2 ClassSPI2;
-typedef SPI_DMAF4_SPI3 ClassSPI3;
+typedef SPI_DMAF4 SPIClass1;
+typedef SPI_DMAF4_SPI2 SPIClass2;
+typedef SPI_DMAF4_SPI3 SPIClass3;
 
 #elif defined(STM32F3xx)
 #include "SPIDMA_F3XX.h"
 extern SPI_DMAF3 SPI;
-typedef SPI_DMAF3 ClassSPI1;
-typedef SPI_DMAF3_SPI2 ClassSPI2;
-typedef SPI_DMAF3_SPI3 ClassSPI3;
+typedef SPI_DMAF3 SPIClass1;
+typedef SPI_DMAF3_SPI2 SPIClass2;
+typedef SPI_DMAF3_SPI3 SPIClass3;
 
 #elif defined(STM32G4xx)
 #include "SPIDMA_G4XX.h"
 extern SPI_DMAG4 SPI;
-typedef SPI_DMAG4 ClassSPI1;
-typedef SPI_DMAG4_SPI2 ClassSPI2;
-typedef SPI_DMAG4_SPI3 ClassSPI3;
+typedef SPI_DMAG4 SPIClass1;
+typedef SPI_DMAG4_SPI2 SPIClass2;
+typedef SPI_DMAG4_SPI3 SPIClass3;
 
 #elif defined(STM32H5xx)
 #include "SPIDMA_H5XX.h"
 extern SPI_DMAH5 SPI;
-typedef SPI_DMAH5 ClassSPI1;
-typedef SPI_DMAH5_SPI2 ClassSPI2;
-typedef SPI_DMAH5_SPI3 ClassSPI3;
+typedef SPI_DMAH5 SPIClass1;
+typedef SPI_DMAH5_SPI2 SPIClass2;
+typedef SPI_DMAH5_SPI3 SPIClass3;
 
 #elif defined(STM32H7xx)
 #include "SPIDMA_H7XX.h"
 extern SPI_DMAH7 SPI;
-typedef SPI_DMAH7 ClassSPI1;
-typedef SPI_DMAH7_SPI2 ClassSPI2;
-typedef SPI_DMAH7_SPI3 ClassSPI3;
+typedef SPI_DMAH7 SPIClass1;
+typedef SPI_DMAH7_SPI2 SPIClass2;
+typedef SPI_DMAH7_SPI3 SPIClass3;
 
 
 #elif defined(STM32F1xx)
 #include "SPIDMA_F1XX.h"
 extern SPI_DMAF1 SPI;
-typedef SPI_DMAF1 ClassSPI1;
-typedef SPI_DMAF1_SPI2 ClassSPI2;
+typedef SPI_DMAF1 SPIClass1;
+typedef SPI_DMAF1_SPI2 SPIClass2;
 #if defined(SPI3_BASE)
-typedef SPI_DMAF1_SPI3 ClassSPI3;
+typedef SPI_DMAF1_SPI3 SPIClass3;
 #endif
 
 #else
 #include "SPIBasic.h"
 extern SPIBasic SPI;
-typedef SPIBasic ClassSPI1;
-typedef SPIBasic ClassSPI2;
-typedef SPIBasic ClassSPI3;
+typedef SPIBasic SPIClass1;
+typedef SPIBasic SPIClass2;
+typedef SPIBasic SPIClass3;
 
 
 #endif
